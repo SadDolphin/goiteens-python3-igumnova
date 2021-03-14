d288c502b05f42fc6f374e7305cb7cda208798c1
Sad Dolfin <79790600+SadDolphin@users.noreply.github.com>
2021-03-14 10:24:29 +0200

    Add files via upload

---
+++ Untitled-1дздздз,,,дз4.py
@@ -0,0 +1,12 @@
+George_Washington = 2
+Thomas_Jefferson = 2
+Herbert_Hoover = 1
+Barack_Obama = 2
+how_years1 = George_Washington * 4
+how_years2 = Thomas_Jefferson * 4
+how_years3 = Herbert_Hoover * 4
+how_years4 = Barack_Obama * 4
+print(how_years1)
+print(how_years2)
+print(how_years3)
+print(how_years4)
