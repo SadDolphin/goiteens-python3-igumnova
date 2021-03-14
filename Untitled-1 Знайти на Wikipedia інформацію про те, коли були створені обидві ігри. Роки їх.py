6ce5c94e54052b77833714363ddb71708430c4cd
Sad Dolfin <79790600+SadDolphin@users.noreply.github.com>
2021-03-14 10:50:45 +0200

    Add files via upload

---
+++ Untitled-1 Знайти на Wikipedia інформацію про те, коли були створені обидві ігри. Роки їх.py
@@ -0,0 +1,10 @@
+minecraft = 11 #номер месяца
+Terraria = 5 #номер месяца
+if Terraria > minecraft:
+    dif = Terraria - minecraft
+    print(dif)
+    print("терария була віпущена раньше на ", dif, "місяців")
+else :
+    dif = minecraft - Terraria
+    print(dif)
+    print("майнкрафт був випущен раньше на", dif ,"місяців")
