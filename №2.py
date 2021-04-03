970e05bd5140115abb854a077ba8e053f43e88d1
Sad Dolphin <79790600+SadDolphin@users.noreply.github.com>
2021-04-03 23:03:09 +0300

    Add files via upload

--- №2.py
+++ №2.py
@@ -1,2 +1,4 @@
-list = ["ярослав", "катя" , "Настя","Лілія" , "Олег"]
-print(list[0])
+we_need =["Гітарист", "Барабанщик", "Піаніст", "Вокаліст"]
+members = len(we_need)
+spend_for_1_year = members * 12 * 100 
+print(spend_for_1_year)
