0dad23f5474ac1a58aeabd31ccf89e87e97f2bc0
Sad Dolphin <79790600+SadDolphin@users.noreply.github.com>
2021-04-10 16:36:43 +0300

    Add files via upload

--- №2.py
+++ №2.py
@@ -1,4 +1,8 @@
-we_need =["Гітарист", "Барабанщик", "Піаніст", "Вокаліст"]
-members = len(we_need)
-spend_for_1_year = members * 12 * 100 
-print(spend_for_1_year)
+car = {
+    "Оптімус Прайм": "Грузовик Peterbilt 379",
+    "Бамблбі": "Chevrolet Camaro",
+    "Джаз": "Porsche 935 Turbo"
+}
+key = "Оптімус Прайм"
+if key in car :
+    print("Оптімус Прайм прибув")
--- №5.py
+++ №5.py
@@ -1,5 +1,5 @@
 cars =  {"Оптімус Прайм" : "Грузовик Peterbilt 379",
        "Бамблбі" : "Chevrolet Camaro",
         "Джаз" : "Porsche 935 Turbo"}
-for x in cars :
-    print(x)
+cars["Сентінел Прайм"] = "Пожежна машина"
+print(cars)
